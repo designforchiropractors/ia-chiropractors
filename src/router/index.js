dbb5283e96f1b65133a24037f7f478c154bc122e
Cody Eklov <cody.eklov@gmail.com>
2022-02-21 07:54:39 -0600

    fix history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
 import Apply from "../views/Apply.vue";
 import Blog from "../views/Blog.vue";
@@ -64,7 +64,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
 });
 
