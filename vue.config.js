6630c27dc85e47ed6e750d1f205aadb87368fc33
Cody Eklov <cody.eklov@gmail.com>
2022-06-02 22:15:57 -0500

    add application form to pre-render

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
          path.resolve(__dirname, 'dist'),
          // List of routes to prerender
          [
-            '/', '/apply', '/blog', '/contact', '/directory', '/privacy-policy', '/terms-conditions', '/notfound',
+            '/', '/apply', '/blog', '/contact', '/directory', '/privacy-policy', '/terms-conditions', '/application-form', '/notfound',
             '/blog/reasons-business-should-be-listed-online-directory', '/blog/simple-ways-to-grow-chiropractor-business-online',
             '/blog/why-netlify-the-simple-fast-and-secure-platform-for-developers', '/blog/top-standard-color-schemes-for-website-design',
             '/blog/top-standard-color-schemes-for-website-design', '/blog/quick-information-on-des-moines-chiropractors-in-2022'
