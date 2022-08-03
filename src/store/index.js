fc8bb473906305a301a6182720461a84e2e356af
Cody Eklov <cody.eklov@gmail.com>
2022-08-03 11:11:20 -0500

    put cb in alpha order

--- src/store/index.js
+++ src/store/index.js
@@ -12,20 +12,20 @@ export default createStore({
         class: 'absolute top-0 left-0 right-0 h-full opacity-70 group-hover:opacity-80 duration-300 group-hover:bg-deeper-red z-10 bg-deep-red',
       },
       {
-        text: 'Design for Chiropractors',
-        link: 'https://www.designforchiropractors.com/',
-        logo: '@/assets/design-for-chiropractors.svg',
-        image: 'design-for-chiropractors.jpg',
-        description: 'Web design, SEO, marketing, and outreach with a focus on serving chiropractors',
-        class: 'absolute top-0 left-0 right-0 h-full opacity-70 group-hover:opacity-80 duration-300 group-hover:bg-blue-800 z-10 bg-blue-600',
-      },
-      {
         text: 'Council Bluffs Chiropractors',
         link: 'https://www.councilbluffschiropractors.com/',
         logo: '@/assets/council-bluffs-chiropractors.svg',
         image: 'council-bluffs-chiropractors.jpg',
         description: 'Find top chiropractors in Council Bluffs, Iowa to manage pain, prevent injury, and start living again',
         class: 'absolute top-0 left-0 right-0 h-full opacity-70 group-hover:opacity-80 duration-300 group-hover:bg-deep-blue z-10 bg-tealish',
+      },
+      {
+        text: 'Design for Chiropractors',
+        link: 'https://www.designforchiropractors.com/',
+        logo: '@/assets/design-for-chiropractors.svg',
+        image: 'design-for-chiropractors.jpg',
+        description: 'Web design, SEO, marketing, and outreach with a focus on serving chiropractors',
+        class: 'absolute top-0 left-0 right-0 h-full opacity-70 group-hover:opacity-80 duration-300 group-hover:bg-blue-800 z-10 bg-blue-600',
       },
       {
         text: 'DSM Chiropractors',
